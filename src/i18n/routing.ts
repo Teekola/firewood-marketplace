5cbf5bf4fbf1bd12f3c6f5196965c9df98052160
Teemu Kerkola <tokerk@utu.fi>
2024-09-30 20:13:11 +0300

    fix: attempt to solve timeout issue in prod

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -8,7 +8,7 @@ const isProduction = env.NODE_ENV === "production";
 export const routing = defineRouting({
    locales: ["fi", "en"], // A list of all locales that are supported
    localePrefix: "as-needed", // only include locale prefix for non-default locales @link https://next-intl-docs.vercel.app/docs/routing#locale-prefix-always
-   defaultLocale: "en", // Used when no locale matches
+   defaultLocale: "fi", // Used when no locale matches
    domains: isProduction
       ? [
            {
