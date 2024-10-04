c22bc3355869679a0e65cd255719ae19bb7c84c9
Teemu Kerkola <tokerk@utu.fi>
2024-10-04 12:51:52 +0300

    test: setup vitest with jest-dom

--- src/components/language-toggle-group.test.tsx
+++ src/components/language-toggle-group.test.tsx
@@ -1,5 +1,36 @@
+import { render } from "@testing-library/react";
 import * as nextIntl from "next-intl";
-import { afterEach, beforeEach, describe, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+import { LanguageToggleGroup } from "./language-toggle-group";
+
+// Note! The values for "@/i18n/routing" must be defined within the mock
+type TestLocale = "fi" | "en";
+
+const translationsByLocale = {
+   fi: {
+      "Select language": "Valitse kieli",
+   },
+   en: {
+      "Select language": "Select language",
+   },
+} as Record<TestLocale, Record<string, string>>;
+
+function mockUseTranslationsForLocale(locale: TestLocale) {
+   const translations = translationsByLocale[locale];
+   return vi.mocked(nextIntl.useTranslations).mockImplementation(() => {
+      return Object.assign(
+         <TargetKey extends string>(key: TargetKey): string => {
+            return translations[key] ?? key;
+         },
+         {
+            rich: (key: string) => key,
+            markup: (key: string) => key,
+            raw: (key: string) => key,
+         }
+      );
+   });
+}
 
 vi.mock("next-intl", () => ({
    useLocale: vi.fn(),
@@ -19,7 +50,6 @@ vi.mock("@/i18n/routing", () => ({
 describe("LanguageToggleGroup", () => {
    beforeEach(() => {
       vi.clearAllMocks();
-      vi.mocked(nextIntl.useLocale).mockReturnValue("en");
    });
 
    afterEach(() => {
@@ -27,14 +57,19 @@ describe("LanguageToggleGroup", () => {
    });
 
    it("renders the toggle group with correct options", () => {
-      // render(<LanguageToggleGroup />);
+      vi.mocked(nextIntl.useLocale).mockReturnValue("en");
+      mockUseTranslationsForLocale("en");
+      const { getByLabelText } = render(<LanguageToggleGroup />);
+
+      const toggleGroup = getByLabelText("Select language");
+      expect(toggleGroup).toBeInTheDocument();
    });
 
    it("sets the default value based on the current locale", () => {
-      // render(<LanguageToggleGroup />);
+      render(<LanguageToggleGroup />);
    });
 
    it("calls the router.replace with correct arguments on locale change", () => {
-      // render(<LanguageToggleGroup />);
+      render(<LanguageToggleGroup />);
    });
 });
