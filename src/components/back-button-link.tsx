9ccc741afc65c96665f8a95b26d7be2e62241220
Teemu Kerkola <tokerk@utu.fi>
2024-11-08 15:35:42 +0200

    feat: add scroll false to backbutton link

--- src/components/back-button-link.tsx
+++ src/components/back-button-link.tsx
@@ -21,7 +21,7 @@ export function BackButtonLink({ label, href, ...props }: Readonly<BackButtonLin
          type="button"
          className={cn("group inline-flex items-center justify-between gap-2", props.className)}
       >
-         <Link href={href}>
+         <Link href={href} scroll={false}>
             <ArrowLeftIcon className="h-4 w-4 transition-transform group-hover:-translate-x-1" />
             {label}
          </Link>
