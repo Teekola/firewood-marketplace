be8e7b67a6c2e53f5b687eb1200678a44bdc67c7
Teemu Kerkola <tokerk@utu.fi>
2024-11-11 11:21:54 +0200

    feat: add translation to country search filter, refactor the field into its own file

--- src/app/[locale]/request-offers/delivery/delivery-form.tsx
+++ src/app/[locale]/request-offers/delivery/delivery-form.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CaretSortIcon } from "@radix-ui/react-icons";
-import { CheckIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { DefaultValues, useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { BackButtonLink } from "@/components/back-button-link";
 import { Button } from "@/components/ui/button";
-import {
-   Command,
-   CommandEmpty,
-   CommandGroup,
-   CommandInput,
-   CommandItem,
-   CommandList,
-} from "@/components/ui/command";
 import {
    Form,
    FormControl,
@@ -26,10 +16,8 @@ import {
    FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { RadioGroup } from "@/components/ui/radio-group";
 import { useRouter } from "@/i18n/routing";
-import { cn } from "@/lib/utils";
 
 import { FormStoreSyncManager } from "../(components)/form-store-sync-manager";
 import { RadioGroupItemCard } from "../(components)/radio-group-item-card";
@@ -39,6 +27,7 @@ import {
    useLastUnlockedStep,
    useSetDeliveryData,
 } from "../store/request-offers-store-provider";
+import { CountryField } from "./country-field";
 import { PostalCodeField } from "./postal-code-field";
 
 export const deliveryFormSchema = z
@@ -65,8 +54,6 @@ export const deliveryFormSchema = z
       }
    );
 export type DeliveryData = z.infer<typeof deliveryFormSchema>;
-
-const countries = [{ label: "Finland", value: "FI" }] as const;
 
 export function DeliveryForm() {
    const deliveryData = useDeliveryData();
@@ -132,78 +119,7 @@ export function DeliveryForm() {
                   )}
                />
 
-               <FormField
-                  control={form.control}
-                  name="country"
-                  render={({ field }) => (
-                     <FormItem className="flex flex-col">
-                        <FormLabel>{t("request-offers.Country")}</FormLabel>
-                        <Popover>
-                           <PopoverTrigger asChild>
-                              <FormControl>
-                                 <Button
-                                    variant="outline"
-                                    role="combobox"
-                                    className={cn(
-                                       "justify-between",
-                                       !field.value && "text-muted-foreground"
-                                    )}
-                                 >
-                                    {field.value
-                                       ? t(
-                                            `countries.${
-                                               countries.find(
-                                                  (country) => country.value === field.value
-                                               )?.label
-                                            }`
-                                         )
-                                       : t("request-offers.Select country")}{" "}
-                                    {field.value && `– ${field.value}`}
-                                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                 </Button>
-                              </FormControl>
-                           </PopoverTrigger>
-                           <PopoverContent className="max-h-[--radix-popover-content-available-height] w-[--radix-popover-trigger-width] p-0">
-                              <Command>
-                                 <CommandInput
-                                    placeholder={t("request-offers.Search country")}
-                                    className="h-9"
-                                 />
-                                 <CommandList>
-                                    <CommandEmpty>
-                                       {t("request-offers.No country found")}
-                                    </CommandEmpty>
-                                    <CommandGroup>
-                                       {countries.map((country) => (
-                                          <CommandItem
-                                             value={country.label}
-                                             key={country.value}
-                                             onSelect={() => {
-                                                form.setValue("country", country.value);
-                                                form.setValue("countryName", country.label);
-                                             }}
-                                          >
-                                             {`${t(`countries.${country.label}`)} – ${country.value}`}
-                                             <CheckIcon
-                                                className={cn(
-                                                   "ml-auto h-4 w-4",
-                                                   country.value === field.value
-                                                      ? "opacity-100"
-                                                      : "opacity-0"
-                                                )}
-                                             />
-                                          </CommandItem>
-                                       ))}
-                                    </CommandGroup>
-                                 </CommandList>
-                              </Command>
-                           </PopoverContent>
-                        </Popover>
-                        <FormMessage />
-                     </FormItem>
-                  )}
-               />
-
+               <CountryField />
                <PostalCodeField />
 
                {deliveryMethod === "homeDelivery" && (
