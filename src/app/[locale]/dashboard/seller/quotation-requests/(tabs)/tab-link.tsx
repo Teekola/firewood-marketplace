45e6d5765e171ca7f3252597ce075b84c62d1337
Teemu Kerkola <tokerk@utu.fi>
2025-02-20 11:59:03 +0200

    fix: do not make tablink highlighted when within it but only when it matches exactly

--- src/app/[locale]/dashboard/seller/quotation-requests/(tabs)/tab-link.tsx
+++ src/app/[locale]/dashboard/seller/quotation-requests/(tabs)/tab-link.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { Link, Pathname, StaticPathname, usePathname } from "@/i18n/routing";
+import { Link, Pathname, usePathname } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
 
 export function TabLink({ href, label }: Readonly<{ href: Pathname; label: string }>) {
    const pathname = usePathname();
 
-   const isWithinRoute = pathname.startsWith(href);
    return (
       <Link
-         href={href as StaticPathname}
+         href={{ pathname: href, params: { id: "" } }}
          className={cn(
             "w-full rounded px-2 py-2 text-center text-sm font-semibold text-muted-foreground transition",
-            isWithinRoute && "bg-card text-card-foreground shadow"
+            pathname === href && "bg-card text-card-foreground shadow"
          )}
       >
          {label}
