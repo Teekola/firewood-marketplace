810eed58278c48e7dcbc419f42bfd05c6d8fb69c
Teemu Kerkola <tokerk@utu.fi>
2025-04-15 09:09:43 +0300

    fix: google button styling

--- src/components/auth/google-register-button.tsx
+++ src/components/auth/google-register-button.tsx
@@ -10,7 +10,12 @@ import { GoogleLogo } from "./google-logo";
 export function GoogleRegisterButton() {
    const t = useTranslations("auth");
    return (
-      <Button variant="outline" size="lg" onClick={() => signIn("google")}>
+      <Button
+         variant="outline"
+         className="bg-[#fff] font-[Roboto] text-[#1f1f1f] hover:bg-[#efefef]"
+         size="lg"
+         onClick={() => signIn("google")}
+      >
          <GoogleLogo className="mr-2" />
          {t("Register with Google")}
       </Button>
--- src/components/auth/google-sign-in-button.tsx
+++ src/components/auth/google-sign-in-button.tsx
@@ -10,7 +10,12 @@ import { GoogleLogo } from "./google-logo";
 export function GoogleSignInButton() {
    const t = useTranslations("auth");
    return (
-      <Button variant="outline" size="lg" onClick={() => signIn("google")}>
+      <Button
+         variant="outline"
+         className="bg-[#fff] font-[Roboto] text-[#1f1f1f] hover:bg-[#efefef]"
+         size="lg"
+         onClick={() => signIn("google")}
+      >
          <GoogleLogo className="mr-2" />
          {t("Sign in with Google")}
       </Button>
