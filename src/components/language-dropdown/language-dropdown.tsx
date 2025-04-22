3bcdd021d29d5b9a44b4e52453fa39db77ee2eb6
Teemu Kerkola <tokerk@utu.fi>
2025-04-22 12:18:53 +0300

    ci: update pre-push script

--- src/components/language-dropdown/language-dropdown.tsx
+++ src/components/language-dropdown/language-dropdown.tsx
@@ -35,7 +35,6 @@ export function LanguageDropdown({
    const t = useTranslations("settings.preferences");
 
    const changeLocale = (locale: Locale) => {
-      console.log("DEBUG");
       if (!locale) return; // Ensure that there is always a value
       // setValue(locale);
       // @ts-expect-error -- TypeScript will validate that only known `params`
