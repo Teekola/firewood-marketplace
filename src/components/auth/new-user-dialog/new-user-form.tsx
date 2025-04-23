d42d970a92e4405060ebef78590eeb5a2e8bc4da
Teemu Kerkola <tokerk@utu.fi>
2025-04-23 11:23:34 +0300

    feat: use checkbox group item card components in new-user-form

--- src/components/auth/new-user-dialog/new-user-form.tsx
+++ src/components/auth/new-user-dialog/new-user-form.tsx
@@ -1,50 +1,37 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 // import { signIn } from "next-auth/react";
 import { useTranslations } from "next-intl";
-import { DefaultValues, useForm, useFormContext } from "react-hook-form";
+import { DefaultValues, useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
+import { CheckboxGroupItemCard } from "@/components/ui/checkbox-group-item-card";
 import { DialogFooter } from "@/components/ui/dialog";
-import {
-   Form,
-   FormControl,
-   FormField,
-   FormItem,
-   FormLabel,
-   FormMessage,
-} from "@/components/ui/form";
+import { Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useRouter } from "@/i18n/routing";
 
 import { registerUser } from "./actions";
 
-export const newUserFormSchema = z
-   .object({
-      isSeller: z.boolean(),
-      isBuyer: z.boolean(),
-   })
-   .refine(
-      (v) => {
-         if (!v.isSeller && !v.isBuyer) {
-            return false;
-         }
-         return true;
-      },
-      {
-         message: "Please select at least one option",
-         path: ["isBuyer"],
-      }
-   );
+const userIntention = {
+   BUYING: "BUYING",
+   SELLING: "SELLING",
+} as const;
+
+const userIntentions = Object.keys(userIntention) as (keyof typeof userIntention)[];
+
+export const newUserFormSchema = z.object({
+   userIntentions: z
+      .array(z.nativeEnum(userIntention))
+      .nonempty({ message: "Please select at least one option" }),
+});
 
 export type NewUserFormData = z.infer<typeof newUserFormSchema>;
 
 export function NewUserForm({ userId }: Readonly<{ userId: string }>) {
    const defaultValues: DefaultValues<NewUserFormData> = {
-      isBuyer: false,
-      isSeller: false,
+      userIntentions: [],
    };
 
    const form = useForm<NewUserFormData>({
@@ -52,13 +39,16 @@ export function NewUserForm({ userId }: Readonly<{ userId: string }>) {
       defaultValues,
    });
 
-   const t = useTranslations("new-user");
+   const t = useTranslations();
    const router = useRouter();
 
    const canProceed = form.formState.isValid;
 
    async function onSubmit(data: NewUserFormData) {
-      const result = await registerUser({ userId, isSeller: data.isSeller });
+      const result = await registerUser({
+         userId,
+         isSeller: data.userIntentions.includes(userIntention.SELLING),
+      });
       console.log("Registered user", result.id);
       router.refresh();
    }
@@ -69,52 +59,42 @@ export function NewUserForm({ userId }: Readonly<{ userId: string }>) {
             onSubmit={form.handleSubmit(onSubmit)}
             className="flex h-full max-w-lg flex-1 flex-col justify-between gap-4"
          >
-            <p>
-               {t("I intend to use the platform for")}
-               {"..."}
-            </p>
-            <div className="flex gap-2">
-               <ToggleField name="isBuyer" label={t("For buying")} />
-               <ToggleField name="isSeller" label={t("For selling")} />
-            </div>
+            <FormField
+               control={form.control}
+               name="userIntentions"
+               render={({ field }) => (
+                  <FormItem className="mb-8 space-y-1">
+                     <FormLabel>
+                        {t("new-user.I intend to use the platform for")}
+                        {"... "}
+                     </FormLabel>
+                     <div className="grid grid-cols-2 gap-2">
+                        {userIntentions.map((userIntention) => (
+                           <CheckboxGroupItemCard
+                              key={userIntention}
+                              label={t(`new-user.${userIntention}`)}
+                              checked={field.value.includes(userIntention)}
+                              onChange={() =>
+                                 field.onChange(
+                                    field.value.includes(userIntention)
+                                       ? field.value.filter((v) => v !== userIntention) // Remove if it was checked
+                                       : [...field.value, userIntention] // Add if it was not checked
+                                 )
+                              }
+                           />
+                        ))}
+                     </div>
+                     <FormMessage />
+                  </FormItem>
+               )}
+            />
 
             <DialogFooter>
                <Button type="submit" size="lg" className="w-full" data-disabled={!canProceed}>
-                  {t("Continue")}
+                  {t("actions.Continue")}
                </Button>
             </DialogFooter>
          </form>
       </Form>
-   );
-}
-
-function ToggleField({ name, label }: { name: keyof NewUserFormData; label: string }) {
-   const form = useFormContext<NewUserFormData>();
-
-   return (
-      <FormField
-         control={form.control}
-         name={name}
-         render={({ field }) => (
-            <FormItem className="w-full space-y-1" {...field}>
-               <FormLabel className="[&:has(:focus-visible)>div]:outline [&:has(:focus-visible)>div]:outline-offset-4 [&:has(:focus-visible)>div]:outline-primary [&:has([data-state=checked])>div]:border-primary">
-                  <FormControl>
-                     <CheckboxPrimitive.Root
-                        checked={field.value}
-                        onCheckedChange={(checked) => {
-                           field.onChange(checked);
-                           form.clearErrors();
-                        }}
-                        className="sr-only"
-                     />
-                  </FormControl>
-                  <div className="flex h-28 w-full cursor-pointer items-center justify-center rounded-md border-4 border-muted bg-secondary p-1 capitalize hover:border-accent">
-                     {label}
-                  </div>
-                  <FormMessage className="mt-2" />
-               </FormLabel>
-            </FormItem>
-         )}
-      />
    );
 }
