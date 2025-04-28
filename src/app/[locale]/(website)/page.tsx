e070e00c4d5ed4babc6e904c22f52585f543d908
Teemu Kerkola <tokerk@utu.fi>
2025-04-28 13:35:56 +0300

    feat: add schema markup for home page

--- src/app/[locale]/(website)/page.tsx
+++ src/app/[locale]/(website)/page.tsx
@@ -1,9 +1,12 @@
 import { Metadata } from "next";
 
 import { getTranslations, setRequestLocale } from "next-intl/server";
+import { Action, Offer, WebPage } from "schema-dts";
 
-import { Link, Locale, routing } from "@/i18n/routing";
+import { env } from "@/env/client";
+import { Link, Locale, localeToLanguageName, routing } from "@/i18n/routing";
 import { authWithSeller } from "@/lib/auth/auth";
+import { JsonLd } from "@/lib/utils/json-ld";
 import { Button } from "@/ui/button";
 
 export function generateStaticParams() {
@@ -28,30 +31,125 @@ export default async function Home({ params }: Readonly<{ params: Promise<{ loca
    const [{ locale }, auth, t] = await Promise.all([params, authWithSeller(), getTranslations()]);
    setRequestLocale(locale);
 
+   // SchemaMarkup
+   const webPageJsonLd = JsonLd<WebPage>({
+      "@context": "https://schema.org",
+      "@type": "WebPage",
+      url: `${env.NEXT_PUBLIC_BASE_URL}/`,
+      name: t("metadata.page-titles.home"),
+      description: t("metadata.page-descriptions.home"),
+      inLanguage: locale,
+
+      publisher: {
+         "@type": "Organization",
+         name: t("company.name"),
+         url: env.NEXT_PUBLIC_BASE_URL,
+         // TODO: Add logo
+         // logo: `${env.NEXT_PUBLIC_BASE_URL}/logo.png`,
+         // TODO: Add correct contact information
+         contactPoint: {
+            "@type": "ContactPoint",
+            contactType: t("company.customer-service"),
+            telephone: t("company.phone"),
+            email: t("company.email"),
+            areaServed: [
+               { "@type": "Place", name: "FI" },
+               { "@type": "Place", name: "US" },
+            ],
+            availableLanguage: routing.locales.map((locale) => {
+               return localeToLanguageName[locale];
+            }),
+         },
+      },
+
+      mainEntityOfPage: {
+         "@type": "WebPage",
+         "@id": `${env.NEXT_PUBLIC_BASE_URL}/`,
+      },
+   });
+
+   const requestOffersJsonLd = JsonLd<Offer>({
+      "@context": "https://schema.org",
+      "@type": "Offer",
+      url: `${env.NEXT_PUBLIC_BASE_URL}/request-offers/firewood`,
+      name: t("home-page.Request Firewood Offers"),
+      description: t("home-page.request-offers-description"),
+      price: t("home-page.request-offers-service-price"),
+      availability: "https://schema.org/InStock",
+      eligibleRegion: [
+         {
+            "@type": "Place",
+            name: "FI",
+         },
+         {
+            "@type": "Place",
+            name: "US",
+         },
+      ],
+      itemOffered: {
+         "@type": "Service",
+         serviceType: t("home-page.request-offers-service-type"),
+         provider: {
+            "@type": "Organization",
+            name: t("company.name"),
+         },
+      },
+   });
+
+   const startSellingJsonLd = JsonLd<Action>({
+      "@context": "https://schema.org",
+      "@type": "Action",
+      name: t("home-page.Start Selling"),
+      description: t("home-page.start-selling-description"),
+      target: {
+         "@type": "EntryPoint",
+         urlTemplate: `${env.NEXT_PUBLIC_BASE_URL}/register`, // TODO: add a custom page for registering as a seller
+         actionPlatform: "Web",
+      },
+      agent: {
+         "@type": "Organization",
+         name: t("company.name"),
+      },
+   });
+
    return (
-      <main className="mx-auto w-full max-w-screen-xl p-3">
-         <section className="my-5 flex w-full gap-3">
-            <div className="ml-0 flex w-full max-w-screen-sm flex-col gap-3">
-               <h1 className="h1">{t("home-page.hero-title")}</h1>
-               <p className="leading-relaxed text-foreground-muted">
-                  {t("home-page.hero-subheadline")}
-               </p>
-               <div className="mt-2 flex gap-2">
-                  <Button asChild variant="cta" size="lg">
-                     <Link href="/request-offers/firewood">
-                        {t("home-page.Request Firewood Offers")}
-                     </Link>
-                  </Button>
-                  {!auth?.seller && (
-                     <Button asChild variant="outline" size="lg">
-                        <Link href={auth ? "/dashboard" : "/auth/register"}>
-                           {t("home-page.Start Selling")}
+      <>
+         <main className="mx-auto w-full max-w-screen-xl p-3">
+            <section className="my-5 flex w-full gap-3">
+               <div className="ml-0 flex w-full max-w-screen-sm flex-col gap-3">
+                  <h1 className="h1">{t("home-page.hero-title")}</h1>
+                  <p className="leading-relaxed text-foreground-muted">
+                     {t("home-page.hero-subheadline")}
+                  </p>
+                  <div className="mt-2 flex gap-2">
+                     <Button asChild variant="cta" size="lg">
+                        <Link href="/request-offers/firewood">
+                           {t("home-page.Request Firewood Offers")}
                         </Link>
                      </Button>
-                  )}
+                     {!auth?.seller && (
+                        <Button asChild variant="outline" size="lg">
+                           <Link href={auth ? "/dashboard" : "/auth/register"}>
+                              {t("home-page.Start Selling")}
+                           </Link>
+                        </Button>
+                     )}
+                  </div>
                </div>
-            </div>
-         </section>
-      </main>
+            </section>
+         </main>
+         <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(webPageJsonLd) }}
+         />
+         <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(requestOffersJsonLd) }}
+         />
+         <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(startSellingJsonLd) }}
+         />
+      </>
    );
 }
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -148,6 +148,11 @@ export const localeToLocalizedLanguageName: Record<Locale, string> = {
    fi: "Suomi",
 };
 
+export const localeToLanguageName: Record<Locale, string> = {
+   en: "English",
+   fi: "Finnish",
+};
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
