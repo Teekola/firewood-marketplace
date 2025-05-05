958ba64280090763d70b344f0bd3cb2f57e28d3a
Teemu Kerkola <tokerk@utu.fi>
2025-05-05 11:13:57 +0300

    feat: simplify example pSEO

--- src/app/[locale]/(website)/[slug]/page.tsx
+++ src/app/[locale]/(website)/[slug]/page.tsx
@@ -1,14 +1,11 @@
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
-import { Locale, redirect } from "@/i18n/routing";
+import { Locale } from "@/i18n/routing";
 
 export async function generateStaticParams() {
-   return [
-      {
-         slug: "/edullista-polttopuuta-turku",
-         locale: "fi",
-      },
-   ];
+   const slugs = await getAllSlugs();
+   return slugs;
 }
 
 export async function generateMetadata({
@@ -16,21 +13,33 @@ export async function generateMetadata({
 }: {
    params: Promise<{ slug: string; locale: Locale }>;
 }): Promise<Metadata> {
-   const { locale, slug } = await params;
+   const { slug } = await params;
+   const pageData = await getPageBySlug(slug);
    return {
-      title: slug + " " + locale,
-      description: slug,
+      title: pageData.metadata.title,
+      description: pageData.metadata.description,
    };
 }
 
-const fetchDataBySlug = async (
-   slug: string
-): Promise<{
-   slug: string;
-   locale: Locale;
-   hero: { h1: string; sub: string };
-   languages: Record<Locale, string>;
-}> => {
+export default async function Page({
+   params,
+}: {
+   params: Promise<{ slug: string; locale: Locale }>;
+}) {
+   const { slug } = await params;
+   const data = await getPageBySlug(slug);
+
+   if (!data) notFound();
+
+   return (
+      <div>
+         <h1 className="h1">{data.hero.h1}</h1>
+         <p className="p">{data.hero.sub}</p>
+      </div>
+   );
+}
+
+export async function getPageBySlug(slug: string) {
    return {
       slug,
       locale: "fi",
@@ -42,23 +51,13 @@ const fetchDataBySlug = async (
          fi: "fi/edullista-polttopuuta-turku",
          en: "cheap-firewood-turku",
       },
+      metadata: {
+         title: slug,
+         description: slug,
+      },
    };
-};
+}
 
-export default async function Page({
-   params,
-}: {
-   params: Promise<{ slug: string; locale: Locale }>;
-}) {
-   const { slug, locale } = await params;
-   const data = await fetchDataBySlug(slug);
-   if (data.locale !== locale) {
-      redirect({ href: { pathname: "/[slug]", params: { slug: data.languages[locale] } }, locale });
-   }
-   return (
-      <div>
-         <h1 className="h1">{data.hero.h1}</h1>
-         <p className="p">{data.hero.sub}</p>
-      </div>
-   );
+async function getAllSlugs() {
+   return [{ slug: "edullista-polttopuuta-turku", locale: "fi" }];
 }
